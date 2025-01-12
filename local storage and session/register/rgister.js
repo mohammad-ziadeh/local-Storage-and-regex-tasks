713abc1d9eb23e31a439ebef4933f18fef4b281b
mohammad <mohammedziadeh6@gmail.com>
2025-01-12 22:45:33 +0300

    Add files via upload

---
+++ local storage and session/register/rgister.js
@@ -0,0 +1,63 @@
+let email = document.getElementById("email");
+let pass = document.getElementById("password");
+let passCon = document.getElementById("passwordCon");
+let err;
+let btn = document.getElementById("btn");
+btn.disabled = true;
+function req() {
+  let errors = document.querySelectorAll(".error");
+  if (email.value === "") {
+    errors[0].innerHTML = "required";
+  } else {
+    errors[0].innerHTML = "";
+  }
+  if (pass.value === "") {
+    errors[1].innerHTML = "required";
+  } else {
+    errors[1].innerHTML = "";
+  }
+  if (passCon.value !== pass.value) {
+    errors[2].innerHTML = "your passwords are not the same >-<";
+  } else {
+    errors[2].innerHTML = "";
+  }
+  if (email.value !== "" && pass.value !== "" && passCon.value === pass.value) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+function emailValidate() {
+  let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let mail = email.value;
+  let errors = document.querySelectorAll(".error");
+
+  if (!mail) {
+    errors[0].innerHTML = "Email is required";
+  } else if (regex.test(mail)) {
+    errors[0].innerHTML = "";
+  } else {
+    errors[0].innerHTML = "Invalid Email address";
+  }
+}
+email.addEventListener("input", emailValidate);
+
+pass.addEventListener("blur", req);
+passCon.addEventListener("input", req);
+email.addEventListener("blur", () => {
+  let errors = document.querySelectorAll(".error");
+  errors.forEach((errorPop) => (errorPop.innerHTML = ""));
+});
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert("U have successfully registered Yippeee!!!!!!!");
+  setTimeout(() => {
+    window.location.href = "toDo/index.html";
+  }, 1000);
+});
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let emailValue = email.value;
+  localStorage.setItem("email", emailValue);
+});
---
+++ local storage and session/register/toDo/main.js
@@ -0,0 +1,141 @@
+const taskInput = document.getElementById("taskinput");
+const addBtn = document.querySelector("#addTask");
+const taskList = document.getElementById("taskList");
+const delAll = document.getElementById("delAll");
+const allChecked = document.getElementById("checkAll");
+
+taskInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    addBtn.click();
+  }
+});
+
+addBtn.addEventListener("click", () => {
+  addTask();
+  delAll.disabled = false;
+});
+
+delAll.addEventListener("click", () => {
+  delchecked();
+});
+
+allChecked.addEventListener("click", () => {
+  AllChecked();
+});
+
+function delchecked() {
+  const tasks = document.querySelectorAll("li");
+  tasks.forEach((task) => {
+    const taskCheckBox = task.querySelector("input[type=checkbox]");
+    if (taskCheckBox.checked) {
+      task.remove();
+    }
+  });
+}
+
+function AllChecked() {
+  const tasks = document.querySelectorAll("li");
+  tasks.forEach((task) => {
+    const taskCheckBox = task.querySelector("input[type=checkbox]");
+    taskCheckBox.checked = true;
+  });
+}
+
+function addTask() {
+  const inputTask = taskInput.value;
+
+  if (inputTask === "") {
+    let Msg = [
+      "It's empty ಠ╭╮ಠ",
+      "Please enter something (´・ω・`)",
+      "So... No tasks? (･ัω･ั)?",
+      "I'm watching IT'S STILL EMPTY ಠ╭╮ಠ",
+      "Ok just ignore me like everyone T-T",
+      "Still nothing >-<",
+    ];
+    window.alert(Msg[Math.floor(Math.random() * Msg.length)]);
+    return;
+  }
+
+  const newInputSpan = document.createElement("input");
+  const listItem = document.createElement("li");
+  const inputSpan = document.createElement("span");
+  const completedBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+  const editBtn = document.createElement("button");
+  const check = document.createElement("input");
+
+  completedBtn.addEventListener("click", () => {
+    toggleComplete(inputSpan);
+  });
+
+  editBtn.addEventListener("click", () => {
+    inputSpan.innerHTML = newInputSpan;
+    listItem.removeChild(inputSpan);
+    listItem.appendChild(newInputSpan);
+  });
+
+  newInputSpan.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      listItem.removeChild(newInputSpan);
+      listItem.appendChild(inputSpan);
+      inputSpan.innerText = newInputSpan.value;
+    }
+  });
+
+  deleteBtn.addEventListener("click", () => {
+    deleteTask(listItem);
+  });
+
+  newInputSpan.value = inputTask;
+  completedBtn.classList.add("btn");
+  completedBtn.classList.add("btn-success");
+
+  inputSpan.classList.add("inputSpan");
+
+  newInputSpan.classList.add("newInput");
+
+  editBtn.classList.add("btn");
+  editBtn.classList.add("btn-warning");
+  editBtn.style.color = "white";
+
+  deleteBtn.classList.add("btn");
+  deleteBtn.classList.add("btn-danger");
+
+  listItem.classList.add("mb-4");
+
+  inputSpan.innerText = inputTask;
+  completedBtn.innerText = "complete";
+  deleteBtn.innerText = "delete";
+  editBtn.innerText = "edit";
+
+  check.type = "checkbox";
+  check.classList.add("check");
+
+  listItem.appendChild(inputSpan);
+  listItem.appendChild(check);
+  listItem.appendChild(completedBtn);
+  listItem.appendChild(editBtn);
+  listItem.appendChild(deleteBtn);
+
+  taskList.appendChild(listItem);
+
+  taskInput.value = "";
+}
+
+function toggleComplete(task) {
+  task.classList.toggle("completed");
+}
+
+function displayName() {
+  let savedEmail = localStorage.getItem("email");
+  if (savedEmail) {
+    let parsedName = JSON.parse(savedEmail);
+    let Name = parsedName[0].split("@")[0];
+    setTimeout(() => {
+      alert("Hello " + Name);
+    }, 2000);
+  }
+}
+window.onload = displayName;
